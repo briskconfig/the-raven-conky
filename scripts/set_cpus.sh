12967fac7f685f04b4a9bbce41992c7b920397ac
eeccher <eeccher@cppwind.com>
2025-07-18 12:39:41 -0600

    Better CPU status display

--- scripts/set_cpus.sh
+++ scripts/set_cpus.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 function set-cpus() {
-  core_ct=`cat /proc/cpuinfo | grep 'processor' | tail -n 1 | awk '{print $NF}'`
-  stub=`cat stub/cpu.line`
+  core_ct=$(( `nproc` - 1 ))  # CPU cores (zero-indexed)
+  stub=`cat stub/cpu.line`    # CPU display template
 
-  while [ $core_ct -ge 0 ]
+  while [ $core_ct -gt 0 ]
   do
-    readable=$(($core_ct + 1))
-    line=`echo "$stub" | sed "s/X/$readable/g" | sed "s/Y/$core_ct/g"`
+    left=$(( core_ct - 1 ))                             # The number of the left-display
+    line=`echo "$stub" | sed "s/X/$left/g" | sed "s/Y/$core_ct/g"`
 
-    sed -e '/History/a\' -e "$line" -i the-raven.conf
+    sed -e '/cpugraph/a\' -e "$line" -i the-raven.conf  # Add the adjusted line to the config
 
-    core_ct=$(($core_ct - 1))
+    core_ct=$(( core_ct - 2 ))                          # Decrement by 2 (each row displays status of 2 rows)
   done
 }
 
